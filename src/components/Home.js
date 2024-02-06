3c31d683cd20327b2b26527d076a413689c8b512
Diognet <morgan.up.dn@gmail.com>
2024-02-06 14:17:35 +0300

    Navigating Programmatically

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,13 @@
-export function Home() {
-  return <div>Home Page</div>;
-}
+import { useNavigate } from 'react-router-dom';
+
+export const Home = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div>Home Page</div>
+      <button onClick={() => navigate('order-summary', { replace: true })}>
+        Place Order
+      </button>
+    </>
+  );
+};
