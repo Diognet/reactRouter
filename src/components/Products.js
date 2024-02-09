f5eba9676c5ddc1c44c8afef5f1e56343f330d2f
Diognet <morgan.up.dn@gmail.com>
2024-02-09 18:46:11 +0300

    use Relative Links

--- src/components/Products.js
+++ src/components/Products.js
@@ -8,8 +8,8 @@ export const Products = () => {
         <input type="search" placeholder="Search products" />
       </div>
       <nav>
-        <Link to="featured">Featured</Link>
-        <Link to="new">new</Link>
+        <Link to="/products/featured">Featured</Link>
+        <Link to="/products/new">new</Link>
       </nav>
       <Outlet />
     </>
