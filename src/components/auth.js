0fd433bf147ebb280018c62ac9f0e82ae7677440
Diognet <morgan.up.dn@gmail.com>
2024-02-15 15:44:58 +0300

    Authentication and Protected Routes 3

--- src/components/auth.js
+++ src/components/auth.js
@@ -3,21 +3,22 @@ import { useContext, createContext, useState } from 'react';
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-const login = (user) => {
-  setUser(user);
-};
-const logout = () => {
-  setUser(null);
-};
+  const login = (user) => {
+    setUser(user);
+  };
+  const logout = () => {
+    setUser(null);
+  };
 
-return (
-  <AuthContext.Provider value={{ user, login, logout }}>
-    {children}
-  </AuthContext.Provider>
-);
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 export const useAuth = () => {
-  return useContext(AuthContext);
+  return;
+  useContext(AuthContext);
 };
