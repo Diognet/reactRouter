5384beb4e7f90a06f3eb6d258264f882b8a7fb82
Diognet <morgan.up.dn@gmail.com>
2024-02-15 15:12:30 +0300

    Authentication and Protected Routes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from './auth';
 
-export default function Profile() {
-  return <div>Profile page </div>;
-}
+export const Profile = () => {
+  const auth = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    auth.logout();
+    navigate('/');
+  };
+  return (
+    <div>
+      Welcome {auth.user}
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+export default Profile;
